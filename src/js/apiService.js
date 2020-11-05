4a7cb6f685ee230e26c1259f39d53361a4511d80
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-11-05 20:06:30 +0200

    adds async/await

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,12 +8,16 @@ export default class ImagesApiService {
         this.page = 1;
     }
 
-    fetchImages() {
-    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
-        return fetch(url).then(response => response.json()).then(({ hits}) => {
+    async fetchImages() {
+        const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+        // return fetch(url).then(response => response.json()).then(({ hits}) => {this.incrementPage(); return hits;}); 
+        
+        const response = await fetch(url);
+        const images = await response.json().then(({ hits}) => {
         this.incrementPage();
-        return hits;
-    });
+        return hits; });
+        return images;
+    
     }
 
     incrementPage() {
