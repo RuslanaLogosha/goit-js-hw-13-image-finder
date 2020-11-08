83f47259e57d094ee45bab420f12a2245eeebaf7
Ruslana Logosha <ruslanalogosha@gmail.com>
2020-11-08 18:31:19 +0200

    async/await code refactoring

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -10,14 +10,14 @@ export default class ImagesApiService {
 
     async fetchImages() {
         const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
-        // return fetch(url).then(response => response.json()).then(({ hits}) => {this.incrementPage(); return hits;}); 
+        // code with promises:
+        // return fetch(url).then(response => response.json()).then(({ hits}) => {this.incrementPage(); return hits;});
         
-        const response = await fetch(url);
-        const images = await response.json().then(({ hits}) => {
+        const response = await fetch(url).then(data => data.json()).then(({ hits}) => {
         this.incrementPage();
             return hits;
         });
-        return images;
+        return response;
     }
 
     incrementPage() {
